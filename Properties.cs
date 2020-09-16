17d1e0ada0937c5a0e7ce905d7fdc926c6e8d4f2
Oguz Kayra <67963917+Mr-Plume@users.noreply.github.com>
2020-09-16 19:49:22 +0300

    Properties.cs
    speed , Rotate Speed etc. value, in here.

---
+++ Properties.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+class Properties : MonoBehaviour
+{
+  public static float speed;
+  public static float Rotatespeed;
+}
