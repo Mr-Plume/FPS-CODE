559a886939bc570042f5903e20fd6beb220c78b4
Oguz Kayra <67963917+Mr-Plume@users.noreply.github.com>
2020-09-16 19:59:42 +0300

    Update Properties.cs

--- Properties.cs
+++ Properties.cs
@@ -4,6 +4,6 @@ using System.Collections;
 
 class Properties : MonoBehaviour
 {
-  public static float Speed {get; protected set;} // this Speed value for move (can't set value in other class)
-  public static float RotateSpeed {get; protected set;} // Rotatespeed value for Rotate (can't set value in other class)
+  public static float Speed {get; protected set;} = 0.1f; // this Speed value for move (can't set value in other class)
+  public static float RotateSpeed {get; protected set;} = 10f; // Rotatespeed value for Rotate (can't set value in other class)
 }
