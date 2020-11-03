07316d7b79183a682ebaf7401dc7263fa8886da9
Oguz Kayra <67963917+Mr-Plume@users.noreply.github.com>
2020-11-04 00:16:48 +0300

    Update Fps-Movement.cs

--- Fps-Movement.cs
+++ Fps-Movement.cs
@@ -1,5 +1,4 @@
-/* I update this code */
-
+/* I improving this code */
 
 using System;
 using System.Collections;
@@ -13,12 +12,12 @@ namespace DefaultNamespace // change namespace dont forget
     {
         private float x, z; // x and y value for Vertical And Horizontal move
         private bool jump, crouch, crouchend, isSlide, slideStart , crouchStart;
-        private Vector3 jumpVector = new Vector3(0 , 10 , 0); // jump vector
+        private Vector3 jumpVector = new Vector3(0 , 30 , 0); // jump vector
         private Vector3 crouchScale = new Vector3(1 , 0.5f , 1); // crouch scale
         private Vector3 defaultScale = new Vector3(1 , 1 , 1); // default scale
 
         //speed variable
-        [SerializeField] private float Jumpspeed,speed,slideSpeed; 
+        [SerializeField] private float Jumpspeed,speed; 
         [SerializeField] private Rigidbody rb;
         [SerializeField] private bool jumpReady;
         [SerializeField] private int layerNumber; // add a your layer number for jumping
@@ -28,16 +27,15 @@ namespace DefaultNamespace // change namespace dont forget
             // I freeze rotation in order not to fall.
             rb.constraints = RigidbodyConstraints.FreezeRotation;
 
-            //Friction for sliding
-            floorMaterial.dynamicFriction = 1.4f;
-            floorMaterial.staticFriction = 1.4f;
+            //Friction for movement
+            floorMaterial.dynamicFriction = 2.5f;
+            floorMaterial.staticFriction = 2.5f;
         }
 
         private void Start()
         {
-            speed = 100 * Time.deltaTime; // mainly movement speed value
+            speed = 50 * Time.deltaTime; // mainly movement speed value
             Jumpspeed = 20; // mainly jump speed value
-            slideSpeed = 0; // mainly start sliding speed value
             
             // recommended setting (don't close gravity)
             rb.useGravity = true;
@@ -61,7 +59,7 @@ namespace DefaultNamespace // change namespace dont forget
             MovementF();
             
             //extra gravity for realistic movement
-            rb.AddForce(0 , -10 , 0); // change by yourself
+            rb.AddForce(0 , -20 , 0); // change by yourself
         }
 
         private void MyInputF()
@@ -78,21 +76,29 @@ namespace DefaultNamespace // change namespace dont forget
             
             if(crouch && Input.GetKey(KeyCode.W)) // if player press crouch and w start crouch
             {
-                StartCoroutine(ChangeSlideSpeedC()); // Just 1 second use slide speed.
+                speed = 0;
+                floorMaterial.dynamicFriction = 0.5f;
+                transform.eulerAngles = new Vector3(-10 , 0 , 0);
             }
 
             if (crouchStart) // reduce speed while squatting
             {
-                speed -= 0.2f;
+                transform.localScale = crouchScale;
             }
 
-            if (crouchend){speed += 0.2f;}
+            if (crouchend)
+            {
+                transform.eulerAngles = new Vector3(0 , 0 , 0);
+                speed = 1;
+                floorMaterial.dynamicFriction = 2f;
+                transform.localScale = defaultScale;
+            }
         }
 
         private void MovementF()
         {
             rb.velocity += new Vector3(x, 0, z) * speed; // always use Time.deltaTime for smooth movement
-            rb.AddForce(transform.forward * slideSpeed , ForceMode.VelocityChange);
+            if(crouch && Input.GetKey(KeyCode.W)){rb.AddForce(0 , -20 , 20);}
         }
 
         // Crouch Function
@@ -121,14 +127,7 @@ namespace DefaultNamespace // change namespace dont forget
         {
             jumpReady = false;
         }
-        
-        private IEnumerator ChangeSlideSpeedC()
-        {
-            slideSpeed = 0.7f;
-            yield return new WaitForSeconds(0.2f);
-            slideSpeed = 0;
-        }
     }
     }
     
-// I add counter movement is coming soon
+// i fixed comments and i add a lot of more comments.
