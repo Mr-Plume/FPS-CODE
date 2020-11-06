b14e901a87249ccfa71cb8bf2d3b026e9ec954c6
Oguz Kayra <67963917+Mr-Plume@users.noreply.github.com>
2020-11-06 14:26:45 +0300

    Update Fps-Movement.cs

--- Fps-Movement.cs
+++ Fps-Movement.cs
@@ -1,4 +1,4 @@
-/* I improving this code */
+/* I gonna add comment coming soon */
 
 using System;
 using System.Collections;
@@ -12,9 +12,10 @@ namespace DefaultNamespace // change namespace dont forget
     {
         private float x, z; // x and y value for Vertical And Horizontal move
         private bool jump, crouch, crouchend, isSlide, slideStart , crouchStart;
-        private Vector3 jumpVector = new Vector3(0 , 30 , 0); // jump vector
+        private Vector3 jumpVector = new Vector3(0 , 50 , 0); // jump vector
         private Vector3 crouchScale = new Vector3(1 , 0.5f , 1); // crouch scale
         private Vector3 defaultScale = new Vector3(1 , 1 , 1); // default scale
+        private RotateCamera rotateCamera;
 
         //speed variable
         [SerializeField] private float Jumpspeed,speed; 
@@ -23,20 +24,18 @@ namespace DefaultNamespace // change namespace dont forget
         [SerializeField] private int layerNumber; // add a your layer number for jumping
         [SerializeField] private PhysicMaterial floorMaterial; // for friction
         [SerializeField] private GameObject camera;
+        [SerializeField] private Transform playerTransform;
 
         private void Awake()
         {
-            // I freeze rotation in order not to fall.
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
-
             //Friction for movement
-            floorMaterial.dynamicFriction = 2f;
-            floorMaterial.staticFriction = 2f;
+            floorMaterial.dynamicFriction = 1f;
+            floorMaterial.staticFriction = 1f;
         }
 
         private void Start()
         {
-            speed = 50 * Time.deltaTime; // mainly movement speed value
+            speed = 100 * Time.fixedDeltaTime; // mainly movement speed value
             Jumpspeed = 20; // mainly jump speed value
             
             // recommended setting (don't close gravity)
@@ -61,7 +60,7 @@ namespace DefaultNamespace // change namespace dont forget
             MovementF();
             
             //extra gravity for realistic movement
-            rb.AddForce(0 , -25 , 0); // change by yourself
+            rb.AddForce(0 , -100 , 0); // change by yourself
         }
 
         private void MyInputF()
@@ -79,20 +78,20 @@ namespace DefaultNamespace // change namespace dont forget
 
         private void MovementF()
         {
-            rb.velocity += new Vector3(x, 0, z) * speed; // always use Time.deltaTime for smooth movement
-            if(crouch && Input.GetKey(KeyCode.W)){rb.AddForce(0 , 0 , 30);}
+            rb.velocity += new Vector3(x, 0 , 0) * speed;
+            rb.velocity += transform.forward * (z * speed);
+            if(crouch && Input.GetKey(KeyCode.W)){rb.AddForce(0 , 0 , 5);}
         }
 
         // Crouch Function
         private void CrouchF()
         {
             
-            if(crouch && Input.GetKey(KeyCode.W)) // if player press crouch and w start crouch
+            if(crouch && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.W) && crouch) // if player press crouch and w start crouch
             {
-                camera.transform.eulerAngles = new Vector3(25 , 0 , 0);
-                speed = 0;
+                x = 0;
+                speed = 0.1f;
                 floorMaterial.dynamicFriction = 0.5f;
-                transform.eulerAngles = new Vector3(-20 , 0 , 0);
             }
 
             if (crouchStart && !Input.GetKey(KeyCode.W)) // reduce speed while squatting
@@ -102,10 +101,14 @@ namespace DefaultNamespace // change namespace dont forget
 
             if (crouchend)
             {
-                camera.transform.eulerAngles = new Vector3(0, 0, 0);
-                transform.eulerAngles = new Vector3(0 , 0 , 0);
-                speed = 1;
-                floorMaterial.dynamicFriction = 2f;
+                x = Input.GetAxis("Horizontal");
+
+                for (float i = 0; i < 2.0f; i += 0.1f) // i increase speed but after 1 second 
+                {
+                    speed += 0.1f;
+                }
+                
+                floorMaterial.dynamicFriction = 1f;
                 transform.localScale = defaultScale;
             }
         }
@@ -123,6 +126,4 @@ namespace DefaultNamespace // change namespace dont forget
             jumpReady = false;
         }
     }
-    }
-    
-// i fixed comments and i add a lot of more comments.
+}
